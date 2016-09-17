1c4eb6fba03d1b2e024f42ac778a0b23e9346ea3
Sven Luijten <svenluijten1995@gmail.com>
2016-09-17 12:28:44 +0200

    extend master page on home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,14 @@
-<a href="https://slack.com/oauth/authorize?scope=commands&client_id={{ getenv('SLACK_CLIENT_ID') }}&redirect_uri={{ getenv('SLACK_REDIRECT_URI') }}">
-    <img alt="Add to Slack"
-         height="40"
-         width="139"
-         src="https://platform.slack-edge.com/img/add_to_slack.png"
-         srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
-    />
-</a>
+@extends('master')
+
+@section('title', 'Packagist for Slack')
+
+@section('content')
+    <a href="https://slack.com/oauth/authorize?scope=commands&client_id={{ getenv('SLACK_CLIENT_ID') }}&redirect_uri={{ getenv('SLACK_REDIRECT_URI') }}">
+        <img alt="Add to Slack"
+             height="40"
+             width="139"
+             src="https://platform.slack-edge.com/img/add_to_slack.png"
+             srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"
+        />
+    </a>
+@endsection
