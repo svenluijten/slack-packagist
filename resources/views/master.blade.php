dd3bbad1171a1429c1c419af0af76d3f036e3abe
Sven Luijten <svenluijten1995@gmail.com>
2016-09-17 16:01:34 +0200

    add Google Analytics

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -13,6 +13,8 @@
         <link rel="manifest" href="/manifest.json">
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
         <meta name="theme-color" content="#F28D1A">
+
+        @include('partials.analytics')
     </head>
 
     <body>
