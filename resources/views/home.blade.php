aa91be58433542c13bda4c5a522a9395ee4763d8
Sven Luijten <svenluijten1995@gmail.com>
2016-09-17 15:18:08 +0200

    remove _blank targets

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
     <h1>Packagist Bot for Slack</h1>
     <p>
         Click the button below to add Packagist Bot to your Slack team. This will add a <code>/packagist</code> command
-        to search <a href="//packagist.org" target="_blank">Packagist</a>, the biggest repository of PHP packages directly
+        to search <a href="//packagist.org">Packagist</a>, the biggest repository of PHP packages directly
         from within Slack.
     </p>
 
@@ -26,7 +26,7 @@
     <h2>Usage</h2>
     <p>
         You can search for <code>vendor/package-name</code> or simply give it a search term. It will always
-        return the top result from <a href="//packagist.org" target="_blank">Packagist</a>.
+        return the top result from <a href="//packagist.org">Packagist</a>.
     </p>
 
     <img src="{{ url('images/demo.png') }}">
