7a7908948a9a3caf43d9c8e8a6aa72fdcd648c2a
Sven Luijten <svenluijten1995@gmail.com>
2016-09-23 19:19:50 +0200

    flesh out privacy policy

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -6,7 +6,15 @@
     <h1>Privacy</h1>
 
     <p>
-        Adding this app to your Slack team will not collect any of the data it recieves. In fact, the application
-        is <a href="//github.com/svenluijten/slack-packagist">open source</a> so you can see how it works for yourself!
+        This app does not collect any of the data it recieves. It simply relays whatever you type after <code>/packagist</code>
+        to <a href="//packagist.org">packagist.org</a>'s API and forms a proper reponse to send back to your Slack channel.
+        In fact, the application is <a href="//github.com/svenluijten/slack-packagist">open source</a> so you can see how
+        it works for yourself!
+    </p>
+
+    <p>
+        When using Packagist Bot, Slack provides it with some basic information. You can find out more about this data from
+        <a href="//api.slack.com/slash-commands">their API docs</a>. Packagist Bot does not collect any email addresses,
+        names or any other personal identifyable information.
     </p>
 @endsection
